4823de637c7658824221c88b984c88e3fa560b0f
ashish173 <aitashish173@gmail.com>
2014-01-05 03:26:30 +0530

    notes showing in MainActivity

--- src/com/davidgassner/plainolnotes/MainActivity.java
+++ src/com/davidgassner/plainolnotes/MainActivity.java
@@ -2,18 +2,20 @@ package com.davidgassner.plainolnotes;
 
 import java.util.List;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
 import com.davidgassner.plainolnotes.data.NoteItem;
 import com.davidgassner.plainolnotes.data.NotesDataSource;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends ListActivity {  // ListActivity inherits from Activity class
 
 	private NotesDataSource datasource;   // instantiating the NoteDataSource class
-	
+	List<NoteItem> notesList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     	// ??
@@ -25,8 +27,13 @@ public class MainActivity extends Activity {
         // data store is instantiated from next line 
         // Context is the superclass of Activity class and thus Main Activity class
         datasource = new NotesDataSource(this);  // this for passing in the context of the main activity
+        
+        refreshDisplay();  // this will refresh the notesList a very frequently used method
+        
+        
+        /*  // this commented out code is for testing purpose
         Log.i("NOTES", "datasource has been created");
-        List<NoteItem> notes = datasource.findAll();    // notes contains all the notes from the persistent data storage
+        datasource.findAll();    // notes contains all the notes from the persistent data storage
        
         NoteItem note = notes.get(0);   // 0th gets the first index value
         
@@ -38,15 +45,30 @@ public class MainActivity extends Activity {
         notes = datasource.findAll();
         note = notes.get(0);
         
-        
+         
         // For effective logging in the logcat using the tag "NOTES" with value.
         Log.i("NOTES", note.getKey() + " : " + note.getText() + "we are at the last line"); 
+        */
+        
         
         
     }
 
+    
+    // on call we retrieve all data from persistent data store
+    private void refreshDisplay() {
+		
+    	notesList = datasource.findAll();  // fetch all data
+		// create an adapter that will tell android what & how to display
+    	ArrayAdapter<NoteItem> adapter = 
+				new ArrayAdapter<NoteItem>(this, android.R.layout.simple_list_item_1, notesList); 
+		// attach this adapter to current ListActivity
+    	setListAdapter(adapter);
+		
+	}
 
-    @Override
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
